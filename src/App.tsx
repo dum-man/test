67d9b46041a5f22520af7d2d31c8cfc3a155b592
Duman <duman.abdrakhmanov@outlook.com>
2023-11-25 22:00:39 +0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ const App = () => {
         width: "100vw",
         height: "100vh",
         display: "flex",
+        flexDirection: "column",
+        gap: "30px",
         justifyContent: "center",
         alignItems: "center",
       }}
@@ -20,6 +22,15 @@ const App = () => {
         numInputs={4}
         renderSeparator={<span>-</span>}
         renderInput={(props) => <input {...props} />}
+        inputType="number"
+      />
+      <input
+        type="text"
+        autoComplete="one-time-code"
+        inputMode="numeric"
+        placeholder="Код из sms"
+        pattern="[0-9]*"
+        maxLength={4}
       />
     </div>
   );
